29bb030912bc66863084360a28e8ba60365642d9
Jairo <kidandcat@gmail.com>
2025-04-27 17:51:21 +0200

    Remove config.go file and add /hello endpoint to demo server for basic greeting functionality.

--- demo/main.go
+++ demo/main.go
@@ -14,6 +14,11 @@ func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 
+	// Add handler for /hello endpoint
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
 	log.Println("Serving on :" + port)
 	if err := http.ListenAndServe(":" + port, nil); err != nil {
 		log.Fatal(err)
