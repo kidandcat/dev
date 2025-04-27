3e6bcd773deb175790a3d1e7809566a6b9edf2ed
Jairo <kidandcat@gmail.com>
2025-04-27 22:33:22 +0200

    Enhance Lint function to include formatting checks with go fmt. Return "No errors found" if no issues are detected, improving user feedback and error handling.

--- code.go
+++ code.go
@@ -8,12 +8,25 @@ import (
 
 func Lint(path string) string {
 	path = Path(path)
-
 	dir := filepath.Dir(path)
+
 	command := exec.Command("go", "vet", dir)
 	output, err := command.CombinedOutput()
 	if err != nil && len(output) == 0 {
 		return fmt.Sprintf("Error formatting go file: %s", err)
 	}
-	return string(output)
+	if len(output) > 0 {
+		return string(output)
+	}
+
+	command = exec.Command("go", "fmt", dir)
+	output, err = command.CombinedOutput()
+	if err != nil && len(output) == 0 {
+		return fmt.Sprintf("Error formatting go file: %s", err)
+	}
+	if len(output) > 0 {
+		return string(output)
+	}
+
+	return "No errors found"
 }
