624d3af441b969650250b66dcdc63dede901150b
Jairo <kidandcat@gmail.com>
2025-04-27 17:16:35 +0200

    Update handleChatCompletion to instruct the agent not to ask follow-up questions. Change server port in demo from 8080 to 3000 for consistency with new configuration.

--- demo/main.go
+++ demo/main.go
@@ -8,8 +8,8 @@ import (
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 
-	log.Println("Serving on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Serving on :3000")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatal(err)
 	}
 }
