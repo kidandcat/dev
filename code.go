4b0086b0f2229cebe992a2ef2313b047b98026a0
Jairo <kidandcat@gmail.com>
2025-04-27 17:24:57 +0200

    Refactor Lint function to remove file extension checks, simplifying the implementation. The function now directly executes 'go vet' on the directory of the provided path, improving clarity and maintaining focus on code analysis.

--- code.go
+++ code.go
@@ -9,18 +9,11 @@ import (
 func Lint(path string) string {
 	path = Path(path)
 
-	extension := filepath.Ext(path)
 	dir := filepath.Dir(path)
-
-	switch extension {
-	case ".go":
-		command := exec.Command("go", "vet", dir)
-		output, err := command.CombinedOutput()
-		if err != nil && len(output) == 0 {
-			return fmt.Sprintf("Error formatting go file: %s", err)
-		}
-		return string(output)
+	command := exec.Command("go", "vet", dir)
+	output, err := command.CombinedOutput()
+	if err != nil && len(output) == 0 {
+		return fmt.Sprintf("Error formatting go file: %s", err)
 	}
-
-	return "Linting not supported for this file type"
+	return string(output)
 }
